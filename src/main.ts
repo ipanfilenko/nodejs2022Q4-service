6c7b0d087a817157850171dff27c0586988a007b
ipanfilenko <panfilenko.igor@gmail.com>
2023-02-03 10:40:48 +0300

    feat: upgrade nest.js, support swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { readFile } from 'node:fs/promises';
+import { parse } from 'yaml';
+
 import { AppModule } from './app.module';
+
+const pathToDocFile = './doc/api.yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const docFile = await readFile(pathToDocFile, 'utf8');
+
+  SwaggerModule.setup('doc', app, parse(docFile)); // TODO: perhaps we need to write documentation (not read)
+
   await app.listen(4000);
 }
+
 bootstrap();
