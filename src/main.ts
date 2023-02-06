41cf16e98fcdbbb0f428f2473e0534afbe24a3d7
ipanfilenko <panfilenko.igor@gmail.com>
2023-02-06 16:33:34 +0300

    feat: update README file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as YAML from 'json-to-pretty-yaml';
 
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import { parse } from 'yaml';
 
 import { AppModule } from './app.module';
@@ -12,11 +13,27 @@ const PORT = process.env['PORT'] || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const docFile = await readFile(pathToDocFile, 'utf8'); // TODO: perhaps we need to write documentation (not read)
+  const docFile = await readFile(pathToDocFile, 'utf8');
 
   SwaggerModule.setup('doc', app, parse(docFile));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // TODO: perhaps we need to write documentation (not read)
+  /* for updating yml file
+  const options = new DocumentBuilder()
+    .setTitle("Home Library Service1")
+    .setDescription("Home music library service2")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  const data = YAML.stringify(document);
+
+  writeFile("./doc/api.yaml", JSON.stringify(document));
+  */
+
+  SwaggerModule.setup('doc', app, parse(docFile));
 
   await app.listen(PORT);
 }
