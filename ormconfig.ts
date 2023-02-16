80c4ab3e8dd5b4fa87cbd9baeaabb23f845be23b
ipanfilenko <panfilenko.igor@gmail.com>
2023-02-16 18:41:33 +0300

    feat: setup connection to database

---
+++ ormconfig.ts
@@ -0,0 +1,18 @@
+import { DataSourceOptions } from "typeorm";
+
+const { DB_HOST, DB_PORT = 5432, DB_USER, DB_NAME, DB_PASS} = process.env;
+
+const typeORMConfig: DataSourceOptions = {
+   "type": "postgres",
+   "host": DB_HOST,
+   "port": +DB_PORT,
+   "username": DB_USER,
+   "password": `${DB_PASS}`,
+   "database": DB_NAME,
+   // "entities": ['./src/**/entities/*.entity{.ts,.js}'],
+   // "migrations": [
+   //    "src/migrations/*.{ts,js}"
+   // ],
+};
+
+export default typeORMConfig;
